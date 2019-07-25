4c7a439acfd3a52f105bec5429f8bc74b778c47d
Pedro Henrique <pedro.henrique@concrete.com.br>
2019-07-24 21:39:29 -0300

    Create BuildPlugins on buildSrc module

---
+++ buildSrc/src/main/java/dependencies/BuildPlugins.kt
@@ -0,0 +1,6 @@
+package dependencies
+
+object BuildPlugins {
+    const val KOTLIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:${PluginVersions.KOTLIN}"
+    const val GRADLE = "com.android.tools.build:gradle:${PluginVersions.GRADLE}"
+}
---
+++ buildSrc/src/main/java/dependencies/PluginVersions.kt
@@ -0,0 +1,6 @@
+package dependencies
+
+object PluginVersions {
+    const val KOTLIN = "1.3.21"
+    const val GRADLE = "3.4.2"
+}
