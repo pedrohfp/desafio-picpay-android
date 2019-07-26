3574fd6ec03d74f5ac5923f3b899c4af84424fc8
Pedro Henrique <pedro.henrique@concrete.com.br>
2019-07-25 22:24:13 -0300

    Config ktlint for subprojects

--- buildSrc/src/main/java/dependencies/Dependencies.kt
+++ buildSrc/src/main/java/dependencies/Dependencies.kt
@@ -3,6 +3,7 @@ package dependencies
 object Dependencies {
     const val KOTLIN = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${PluginsVersions.KOTLIN}"
     const val KTX = "androidx.core:core-ktx:${Versions.CORE_KTX}"
+    const val KTLINT = "com.pinterest:ktlint:${Versions.KTLINT}"
 
     object Android {
         const val APPCOMPAT = "androidx.appcompat:appcompat:${Versions.ANDROIDX}"
--- buildSrc/src/main/java/dependencies/Versions.kt
+++ buildSrc/src/main/java/dependencies/Versions.kt
@@ -4,6 +4,7 @@ object Versions {
     const val ANDROIDX = "1.0.2"
     const val CORE_KTX = "1.0.2"
     const val CONSTRAINT_LAYOUT = "1.1.3"
+    const val KTLINT = "0.34.2"
     const val JUNIT = "4.12"
     const val ESPRESSO_RUNNER = "1.2.0"
     const val ESPRESSO_CORE = "3.2.0"
