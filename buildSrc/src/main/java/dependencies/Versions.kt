33713b2a3c21e009cd8409964e7e498f9b095cf2
Pedro Henrique <pedro.henrique@concrete.com.br>
2019-07-25 21:32:51 -0300

    Create commons gradle config for modules

--- buildSrc/src/main/java/dependencies/Versions.kt
+++ buildSrc/src/main/java/dependencies/Versions.kt
@@ -3,7 +3,7 @@ package dependencies
 object Versions {
     const val ANDROIDX = "1.0.2"
     const val CORE_KTX = "1.0.2"
-    const val CONSTRAINT_LAYOUT = "1.0.3"
+    const val CONSTRAINT_LAYOUT = "1.1.3"
     const val JUNIT = "4.12"
     const val ESPRESSO_RUNNER = "1.2.0"
     const val ESPRESSO_CORE = "3.2.0"
