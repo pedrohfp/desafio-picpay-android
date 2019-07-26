235cd5767f03ed068a89761d43265af015b22ba3
Pedro Henrique <pedro.henrique@concrete.com.br>
2019-07-25 21:06:32 -0300

    Config subproject dependencies

---
+++ buildSrc/src/main/java/AndroidConfig.kt
@@ -0,0 +1,9 @@
+object AndroidConfig {
+    const val APPLICATION_ID = "com.example.desafiopicpay"
+    const val COMPILE_SDK = 28
+    const val MIN_SDK = 16
+    const val TARGET_SDK = COMPILE_SDK
+    const val VERSION_CODE = 1
+    const val VERSION_NAME = "1.0"
+    const val TEST_INSTRUMENTATION_RUNNER = "br.com.odete.commonstest.test.di.CustomTestRunner"
+}
