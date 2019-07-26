af8f3d726170224c17716e953a48f49803417cfe
Pedro Henrique <pedro.henrique@concrete.com.br>
2019-07-25 22:40:14 -0300

    Config detekt for subprojects

--- buildSrc/src/main/java/dependencies/BuildPlugins.kt
+++ buildSrc/src/main/java/dependencies/BuildPlugins.kt
@@ -3,4 +3,5 @@ package dependencies
 object BuildPlugins {
     const val KOTLIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:${PluginsVersions.KOTLIN}"
     const val GRADLE = "com.android.tools.build:gradle:${PluginsVersions.GRADLE}"
+    const val DETEKT = "io.gitlab.arturbosch.detekt:detekt-gradle-plugin:${PluginsVersions.DETEKT}"
 }
