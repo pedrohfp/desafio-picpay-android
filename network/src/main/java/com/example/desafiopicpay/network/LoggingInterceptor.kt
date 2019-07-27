8204bad7ac958465822044521aa2995d7894b81b
Pedro Henrique <pedro.henrique@concrete.com.br>
2019-07-27 02:40:35 -0300

    Config network koin module

--- network/src/main/java/com/example/desafiopicpay/network/LoggingInterceptor.kt
+++ network/src/main/java/com/example/desafiopicpay/network/LoggingInterceptor.kt
@@ -1,2 +1,14 @@
 package com.example.desafiopicpay.network
 
+import okhttp3.logging.HttpLoggingInterceptor
+
+class LoggingInterceptor {
+    fun getInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = if (BuildConfig.DEBUG)
+                HttpLoggingInterceptor.Level.BODY
+            else
+                HttpLoggingInterceptor.Level.NONE
+        }
+    }
+}
