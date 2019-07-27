a95b394d0be90ab0ea18f870f074f8b171704d37
Pedro Henrique <pedro.henrique@concrete.com.br>
2019-07-27 02:47:56 -0300

    Create custom application

---
+++ app/src/main/java/com/example/desafiopicpay/DesafioPicpayApplication.kt
@@ -0,0 +1,11 @@
+package com.example.desafiopicpay
+
+import android.app.Application
+
+class DesafioPicpayApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+
+    }
+}
