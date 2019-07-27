dc597ecc895518453eb92d588e5f5e23ef82296a
Pedro Henrique <pedro.henrique@concrete.com.br>
2019-07-27 02:34:03 -0300

    Create LiveDataCallAdapter for retrofit

--- network/src/main/java/com/example/desafiopicpay/network/adapter/LiveDataCallAdapter.kt
+++ network/src/main/java/com/example/desafiopicpay/network/adapter/LiveDataCallAdapter.kt
@@ -1,7 +1,11 @@
-package com.example.desafiopicpay.network
+package com.example.desafiopicpay.network.adapter
 
 import androidx.lifecycle.LiveData
+import retrofit2.Call
 import retrofit2.CallAdapter
+import retrofit2.Callback
+import retrofit2.Response
+import java.lang.reflect.Type
 
 class LiveDataCallAdapter<R>(private val responseType: Type):
     CallAdapter<R, LiveData<ApiResponse<R>>> {
--- network/src/main/java/com/example/desafiopicpay/network/adapter/LiveDataCallAdapterFactory.kt
+++ network/src/main/java/com/example/desafiopicpay/network/adapter/LiveDataCallAdapterFactory.kt
@@ -1,14 +1,20 @@
-package com.example.desafiopicpay.network
+package com.example.desafiopicpay.network.adapter
 
 import retrofit2.CallAdapter
 import retrofit2.Retrofit
 import java.lang.reflect.ParameterizedType
+import java.lang.reflect.Type
 
 class LiveDataCallAdapterFactory: CallAdapter.Factory() {
     override fun get(returnType: Type, annotations: Array<Annotation>, retrofit: Retrofit): CallAdapter<*, *>? {
         val observableType =
             CallAdapter.Factory.getParameterUpperBound(0, returnType as ParameterizedType) as? ParameterizedType
                 ?: throw IllegalArgumentException("resource must be parameterized")
-        return LiveDataCallAdapter<Any>(CallAdapter.Factory.getParameterUpperBound(0, observableType))
+        return LiveDataCallAdapter<Any>(
+            getParameterUpperBound(
+                0,
+                observableType
+            )
+        )
     }
 }
