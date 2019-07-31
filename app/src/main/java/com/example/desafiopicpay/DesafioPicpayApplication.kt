b5e7193e70a7fc0cbab8c852d1d45635eec8277c
Pedro Henrique <pedro.henrique@concrete.com.br>
2019-07-31 00:18:41 -0300

    Config dark theme

--- app/src/main/java/com/example/desafiopicpay/DesafioPicpayApplication.kt
+++ app/src/main/java/com/example/desafiopicpay/DesafioPicpayApplication.kt
@@ -1,12 +1,16 @@
 package com.example.desafiopicpay
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.desafiopicpay.network.networkModule
 import org.koin.core.context.startKoin
 
 class DesafioPicpayApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        AppCompatDelegate.setDefaultNightMode(
+            AppCompatDelegate.MODE_NIGHT_YES)
 
         startKoin {
             modules(networkModule)
