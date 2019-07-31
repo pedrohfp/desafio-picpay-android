854911f662ba3413407533a40fc96a98b093ace3
Pedro Henrique <pedro.henrique@concrete.com.br>
2019-07-31 20:50:57 -0300

    Add Mockito, MockWebServer and Robolectric dependencies on base gradle file

--- buildSrc/src/main/java/dependencies/TestDependencies.kt
+++ buildSrc/src/main/java/dependencies/TestDependencies.kt
@@ -2,6 +2,9 @@ package dependencies
 
 object TestDependencies {
     const val JUNIT = "junit:junit:${Versions.JUNIT}"
+    const val ROBOLECTRIC = "org.robolectric:robolectric:${Versions.ROBOLECTRIC}"
+    const val MOCK_WEB_SERVER = "com.squareup.okhttp3:mockwebserver:${Versions.OKHTTP}"
+    const val MOCKITO_KOTLIN = "com.nhaarman.mockitokotlin2:mockito-kotlin:${Versions.MOCKITO_KOTLIN}"
     const val ESPRESSO_RUNNER = "androidx.test:runner:${Versions.ESPRESSO_RUNNER}"
     const val ESPRESSO_CORE = "androidx.test.espresso:espresso-core:${Versions.ESPRESSO_CORE}"
 }
--- buildSrc/src/main/java/dependencies/Versions.kt
+++ buildSrc/src/main/java/dependencies/Versions.kt
@@ -10,6 +10,8 @@ object Versions {
     const val OKHTTP = "4.0.1"
     const val ARCH_COMPONENTS= "2.0.0"
     const val JUNIT = "4.12"
+    const val ROBOLECTRIC = "4.3"
+    const val MOCKITO_KOTLIN = "2.1.0"
     const val ESPRESSO_RUNNER = "1.2.0"
     const val ESPRESSO_CORE = "3.2.0"
 }
