f327b69eae75640b30bc978d900ed81c679bb235
Pedro Henrique <pedro.henrique@concrete.com.br>
2019-08-03 17:11:47 -0300

    Create home activity and setup with default activity

--- home/src/main/java/com/example/desafiopicpay/home/HomeActivity.kt
+++ home/src/main/java/com/example/desafiopicpay/home/HomeActivity.kt
@@ -1,12 +1,12 @@
-package com.example.desafiopicpay
+package com.example.desafiopicpay.home
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
