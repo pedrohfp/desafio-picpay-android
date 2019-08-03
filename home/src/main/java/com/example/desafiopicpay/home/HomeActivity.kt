ab0931eaad394a96db8dc6375b89abbc9a4a3749
Pedro Henrique <pedro.henrique@concrete.com.br>
2019-08-03 17:25:54 -0300

    Setup viewmodel on home activity

--- home/src/main/java/com/example/desafiopicpay/home/HomeActivity.kt
+++ home/src/main/java/com/example/desafiopicpay/home/HomeActivity.kt
@@ -2,11 +2,32 @@ package com.example.desafiopicpay.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.desafiopicpay.home.data.UserDTO
+import com.example.desafiopicpay.network.ui.ErrorData
+import com.example.desafiopicpay.network.ui.observeOnError
+import com.example.desafiopicpay.network.ui.observeOnSuccess
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class HomeActivity : AppCompatActivity() {
+internal class HomeActivity : AppCompatActivity() {
+
+    private val homeViewModel: HomeViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+
+    private fun setupViewModel() {
+        homeViewModel.loadUserList()
+            .observeOnSuccess(this, ::onSuccess)
+            .observeOnError(this, ::onError)
+    }
+
+    private fun onSuccess(userList: List<UserDTO>) {
+
+    }
+
+    private fun onError(errorData: ErrorData) {
+
     }
 }
